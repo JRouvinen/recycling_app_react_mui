408c2108a2f04a9773fef96b03a484ff1a9bd6ba
rouvju <jmrouvinen@gmail.com>
2023-04-13 21:23:01 +0300

    updates on settings screen

--- src/components/Locations/SettingsBtnComponent.js
+++ src/components/Locations/SettingsBtnComponent.js
@@ -1,15 +1,16 @@
-import React, {useContext} from 'react';
-import PropTypes from 'prop-types';
-import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import DialogTitle from '@mui/material/DialogTitle';
-import Dialog from '@mui/material/Dialog';
-import ServiceFilter from './ServiceFilterComponent';
-import Switch from '@mui/material/Switch';
+import React, { useContext } from "react";
+import PropTypes from "prop-types";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import DialogTitle from "@mui/material/DialogTitle";
+import Dialog from "@mui/material/Dialog";
+import ServiceFilter from "./ServiceFilterComponent";
+import Switch from "@mui/material/Switch";
 import { styled } from "@mui/material/styles";
-import TextField from '@mui/material/TextField';
-import ColumnFilter from './ColumnFilterComponent';
-import SettingsContext from '../../store/settings-context';
+import TextField from "@mui/material/TextField";
+import ColumnFilter from "./ColumnFilterComponent";
+import SettingsContext from "../../store/settings-context";
+import LoadingButton from "@mui/lab/LoadingButton";
 
 const Div = styled("div")(({ theme }) => ({
   ...theme.typography.button,
@@ -17,19 +18,17 @@ const Div = styled("div")(({ theme }) => ({
   padding: theme.spacing(1),
 }));
 
-
 function SimpleDialog(props) {
-  console.log('Settingsbtn')
+  console.log("Settingsbtn");
   const ctx = useContext(SettingsContext);
-  console.log(ctx)
-  
+  console.log(ctx);
+
   // const { onClose, selectedValue, open } = props;
-  const { onClose,  open } = props;
+  const { onClose, open } = props;
 
   const handleClose = () => {
     // onClose(selectedValue);
     onClose();
-
   };
 
   const handleListItemClick = (value) => {
@@ -38,32 +37,40 @@ function SimpleDialog(props) {
 
   const [btn1_checked, setBtn1Checked] = React.useState(ctx.localDatabase);
   const [btn2_checked, setBtn2Checked] = React.useState(false);
-  const [info_txt, setInfoTxt] = React.useState('...')
+  const [info_txt, setInfoTxt] = React.useState("...");
+  const [local_file, setLocalFile] = React.useState("");
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [loadedFile, setLoadedFile] = React.useState();
 
   const handleChange1 = (event) => {
     setBtn1Checked(event.target.checked);
     setBtn2Checked(false);
     ctx.onChangeServer();
     // settingsCtx.setserverDatabase(false)
-    setInfoTxt('Loading local database..')
-
+    setInfoTxt("Loading local database..");
   };
-const handleChange2 = (event) => {
+  const handleChange2 = (event) => {
     setBtn1Checked(false);
     setBtn2Checked(event.target.checked);
     ctx.onChangeServer();
     // settingsCtx.setlocalDatabase(false)
     // settingsCtx.setserverDatabase(true)
-    setInfoTxt('Connecting to server..')
-
+    setInfoTxt("Connecting to server..");
   };
+
+  const uploadHandle = (event) => {
+    setLoadedFile(event);
+    setLocalFile(event.target.value);
+    // let file_name_split = local_file.split("\\")
+    // console.log(file_name_split)
+  }
+
   return (
     <Dialog onClose={handleClose} open={open}>
       <DialogTitle>Program settings</DialogTitle>
-      
-      
+
       <List sx={{ pt: 1 }}>
-      <TextField
+        <TextField
           id="outlined-read-only-input"
           label="Info"
           // defaultValue="Hello World"
@@ -74,28 +81,50 @@ const handleChange2 = (event) => {
           padding="dense"
           sx={{ m: 1, minWidth: 250, maxWidth: 300 }}
         />
-        {/* <br/>
-        <SelectLocationsProcess/> */}
-        <br/>
-        <ServiceFilter/>
-        <br/>
-        <ColumnFilter/>
-        <br/>
+        <br />
+        <br />
+        <ColumnFilter />
+        <br />
+        <TextField
+          id="outlined-read-only-input"
+          label="Local database file"
+          // defaultValue="Hello World"
+          value={local_file}
+          InputProps={{
+            readOnly: true,
+          }}
+          padding="dense"
+          sx={{ m: 1, minWidth: 250, maxWidth: 300 }}
+        />
+        <br />
+        {!isLoading && (
+          <Button
+            variant="contained" onClick={uploadHandle}
+            component="label"
+            sx={{ m: 1, minWidth: 50, maxWidth: 300 }}
+          >
+            Upload
+            <input hidden multiple type="file" />
+          </Button>
+        )}
+        {isLoading && (
+          <LoadingButton loading loadingIndicator="Loading…" variant="outlined">
+            Fetch data
+          </LoadingButton>
+        )}
+
         <Div>Use local database</Div>
         <Switch
-      checked={btn1_checked}
-      onChange={handleChange1}
-      inputProps={{ 'aria-label': 'controlled' }}
-      
-    />
-    <Div>Use server database</Div>
+          checked={btn1_checked}
+          onChange={handleChange1}
+          inputProps={{ "aria-label": "controlled" }}
+        />
+        <Div>Use server database</Div>
         <Switch
-      checked={btn2_checked}
-      onChange={handleChange2}
-      inputProps={{ 'aria-label': 'controlled' }}
-      
-    />
-
+          checked={btn2_checked}
+          onChange={handleChange2}
+          inputProps={{ "aria-label": "controlled" }}
+        />
       </List>
     </Dialog>
   );
@@ -127,7 +156,7 @@ export default function SettingsDialog() {
       </Typography>
       <br /> */}
       <Button color="inherit" onClick={handleClickOpen}>
-Settings
+        Settings
       </Button>
       <SimpleDialog
         // selectedValue={selectedValue}
